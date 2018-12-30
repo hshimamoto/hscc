d6758c6c41cc671bd463a8aa92a2547b8eb2ab28
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-12-30 10:41:49 +0900

    tokenize() and expr()

--- main.c
+++ main.c
@@ -1,5 +1,123 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+
+enum {
+	TK_NUM = 256,
+	TK_EOF,
+};
+
+typedef struct {
+	int type;
+	int val;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p)
+{
+	int i = 0; // index of tokens
+
+	while (*p) {
+		if (isspace(*p)) {
+			p++;
+			continue;
+		}
+		if (*p == '+' || *p == '-') {
+			tokens[i].type = *p;
+			i++;
+			p++;
+			continue;
+		}
+		if (isdigit(*p)) {
+			tokens[i].type = TK_NUM;
+			tokens[i].val = strtol(p, &p, 0);
+			i++;
+			continue;
+		}
+		fprintf(stderr, "syntax error: %s\n", p);
+		exit(1);
+	}
+	tokens[i].type = TK_EOF;
+}
+
+enum {
+	ND_NUM = 256,
+};
+
+typedef struct node {
+	int type;
+	struct node *lhs, *rhs;
+	int val;	// for ND_NUM;
+} Node;
+
+int pos; // position in tokens
+
+Node *new_node(int type, Node *lhs, Node *rhs, int val)
+{
+	Node *node = malloc(sizeof(Node));
+
+	node->type = type;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	node->val = val;
+
+	return node;
+}
+
+/*
+ * expr: num | num "+" expr | num "-" expr
+ */
+Node *num()
+{
+	if (tokens[pos].type == TK_NUM) {
+		int val = tokens[pos].val;
+		pos++;
+		return new_node(ND_NUM, NULL, NULL, val);
+	}
+	// syntax error
+	fprintf(stderr, "syntax error\n");
+	exit(1);
+}
+
+Node *expr()
+{
+	Node *lhs = num();
+
+	if (tokens[pos].type == '+') {
+		pos++;
+		return new_node('+', lhs, expr(), 0);
+	}
+	if (tokens[pos].type == '-') {
+		pos++;
+		return new_node('-', lhs, expr(), 0);
+	}
+	return lhs;
+}
+
+void gen(Node *node)
+{
+	if (node->type == '+') {
+		gen(node->lhs);
+		gen(node->rhs);
+		puts("  pop rdx");
+		puts("  pop rax");
+		puts("  add rax, rdx");
+		puts("  push rax");
+	} else if (node->type == '-') {
+		gen(node->lhs);
+		gen(node->rhs);
+		puts("  pop rdx");
+		puts("  pop rax");
+		puts("  sub rax, rdx");
+		puts("  push rax");
+	} else if (node->type == ND_NUM) {
+		printf("  push %d\n", node->val);
+	} else {
+		fprintf(stderr, "error node->type = %d\n", node->type);
+		exit(1);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -14,35 +132,12 @@ int main(int argc, char **argv)
 	puts(".global main");
 	puts("main:");
 
-	char *endp;
-	long n;
+	tokenize(p);
 
-	n = strtol(p, &endp, 0);
-	if (p == endp) {
-		fprintf(stderr, "syntax error\n");
-		return 1;
-	}
-
-	printf("  mov rax, %d\n", n);
-	p = endp;
-	while (*p) {
-		if (*p == '+')
-			printf("  add");
-		else if (*p == '-')
-			printf("  sub");
-		else {
-			fprintf(stderr, "syntax error\n");
-			return 1;
-		}
-		p++;
-		n = strtol(p, &endp, 0);
-		if (p == endp) {
-			fprintf(stderr, "syntax error\n");
-			return 1;
-		}
-		printf(" rax, %d\n", n);
-		p = endp;
-	}
+	Node *node = expr();
+	// generate stack machine
+	gen(node);
+	puts("  pop rax");
 	puts("  ret");
 
 	return 0;
