2ab8c7e8418af45a6d3aa716004fa51c5ac0deba
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-12-30 09:16:50 +0900

    change try() in test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 try() {
-	ex=$1
-	in=$2
+	in="$1"
+	ex="$2"
 	./hscc "$in" > tmp.s
 	gcc -o tmp tmp.s
 	./tmp
-	ret=$?
+	ret="$?"
 
 	if [ "$ret" != "$ex" ]; then
 		echo "$ex expected, but got $ret"
@@ -16,7 +16,7 @@ try() {
 
 try 0 0
 try 42 42
-try 3 1+2
-try 1 3-2
+try 1+2 3
+try 3-2 1
 
 echo OK
