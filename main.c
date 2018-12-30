a09bf37fbb97e8ff1fb0c6d138b6b0aef681ccca
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-12-30 09:15:32 +0900

    support single '+' and '-'

--- main.c
+++ main.c
@@ -8,10 +8,38 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	puts(".intel_syntax noprefix");
 	puts(".global main");
 	puts("main:");
-	printf("  mov rax, %d\n", atoi(argv[1]));
+
+	char *endp;
+	long n;
+
+	n = strtol(p, &endp, 0);
+	if (p == endp) {
+		fprintf(stderr, "syntax error\n");
+		return 1;
+	}
+
+	printf("  mov rax, %d\n", n);
+	if (*endp != '\0') {
+		p = endp;
+		if (*p != '+' && *p != '-') {
+			fprintf(stderr, "syntax error\n");
+			return 1;
+		}
+		n = strtol(p + 1, &endp, 0);
+		if (p + 1 == endp) {
+			fprintf(stderr, "syntax error\n");
+			return 1;
+		}
+		if (*p == '+')
+			printf("  add rax, %d\n", n);
+		else
+			printf("  sub rax, %d\n", n);
+	}
 	puts("  ret");
 
 	return 0;
--- test.sh
+++ test.sh
@@ -16,5 +16,7 @@ try() {
 
 try 0 0
 try 42 42
+try 3 1+2
+try 1 3-2
 
 echo OK
